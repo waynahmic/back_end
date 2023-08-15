d01294d5e4719ba22fb9a3654b54651caed20c4c
waynahmic <107517481+waynahmic@users.noreply.github.com>
2023-08-15 14:40:17 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -44,7 +44,7 @@ const PORT = 8080
 app.use(express.json()); //has access to our res and req......json format
 app.use(express.urlencoded({extended:false}))
 app.use(cors({
-    origin : ['http://localhost:3000','https://frontend-three-brown-54.vercel.app'],
+    origin : ['http://localhost:3000','https://frontend-lvn3xpg93-waynahmic.vercel.app/'],
     credentials:true
 }
 ))
