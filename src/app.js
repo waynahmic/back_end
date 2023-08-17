fe2b463382e5d9664ef1b0e04fe241fd17b6b2ae
waynahmic <107517481+waynahmic@users.noreply.github.com>
2023-08-17 12:09:11 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -44,7 +44,7 @@ const PORT = 8080
 app.use(express.json()); //has access to our res and req......json format
 app.use(express.urlencoded({extended:false}))
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://frontend-pxy8tjlb9-waynahmic.vercel.app/'],
+    origin: ['http://localhost:3000', 'https://crud-backend-707b.onrender.com/'],
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
